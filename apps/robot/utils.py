0ed8c87cd8693771fcdfe8a50ef3a11ec37be2c4
frederic.juppont <frederic.juppont@gmail.com>
2019-07-14 14:50:28 +0200

    start project

---
+++ apps/robot/utils.py
@@ -0,0 +1,15 @@
+from .words import WORDSLIST
+from textwrap import wrap
+
+
+def parse(question):
+    words = [word for word in question.split() if word not in WORDSLIST]
+    return words
+
+
+def parse_text_wiki(d):
+    res = ''
+    for k in d:
+        if 'extract' in d[k]:
+            res += d[k]['extract']
+    return wrap(res, width=40)
